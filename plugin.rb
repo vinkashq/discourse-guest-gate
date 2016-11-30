7cba837b619c4c80b09ff0732a37fa824a84b114
Vinoth Kannan <vinothkannan@vinkas.com>
2016-11-30 14:03:28 +0530

    version bump to v0.1.0

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,5 @@
 # name: guest-gate
 # about: Plugin to add a custom brand header for Discourse
-# version: 0.0.1
+# version: 0.1.0
 # authors: Vinoth Kannan (vinothkannan@vinkas.com)
 # url: https://github.com/vinothkannans/discourse-guest-gate
