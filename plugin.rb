3d53b56b33f630ee7d4cf6727b37f9f58f5d7bf8
Vinoth Kannan <vinothkannan@vinkas.com>
2016-12-01 16:57:17 +0530

    bump version number

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,5 @@
 # name: guest-gate
 # about: Force guest users to create account by preventing them to see more topics
-# version: 0.1.1
+# version: 0.2.0
 # authors: Vinoth Kannan (vinothkannan@vinkas.com)
 # url: https://github.com/vinothkannans/discourse-guest-gate
